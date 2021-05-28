99f4fe750c9f2ad74631ab31d1009dd9da84e834
Sami Hattab <sami@roomba.io>
2021-05-28 19:11:03 +0200

    add comments for the FrontController

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -8,6 +8,7 @@ use App\Product;
 
 class FrontController extends Controller
 {
+    // Méthode qui transmet les catégories de la base de donnée à la barre de navigation
     public function __construct()
     {
         // méthode pour injecter des données à une vue partielle
@@ -16,26 +17,28 @@ class FrontController extends Controller
             $view->with('categories', $categories); // on passe les données à la vue
         });
     }
-
+    // Méthode qui renvoie la vue de la page d'accueil avec tout les produits avec une pagination de 6
     public function index() {
         $products = Product::paginate(6);
 
         return view("front.index", ["products" => $products]);
     }
-
+    // Méthode qui renvoie la vue détaillé du produit choisi
     public function show(int $id)
     {
         return view('front.product', ['product' => Product::find($id)]);
     }
-
+    // Méthode qui renvoie la vue des pages des catégories séléctionnées
     public function showCategories(int $id)
     {
+        // Récupére les produits associées à la catégorie choisi avec une pagination de 6
         $products = Category::find($id)->products()->paginate(6);
 
         return view('front.category', ['products' => $products, 'category' => Category::find($id)]);
     }
-
+    // Méthode qui renvoie la vue des produits en soldes
     public function showDiscount() {
+        // Récupère les produits dont le champ discount est à true, avec une pagination à 6
         $products = Product::discount()->paginate(6);
 
         return view('front.discount', ['products' => $products]);
