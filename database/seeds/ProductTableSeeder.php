c2f195dd564236798a3cb0f94af5f3d751a87866
Sami Hattab <sami@roomba.io>
2021-05-28 18:57:17 +0200

    add a better way to create the categories and sizes, and add comments

--- database/seeds/ProductTableSeeder.php
+++ database/seeds/ProductTableSeeder.php
@@ -13,35 +13,26 @@ class ProductTableSeeder extends Seeder
     public function run()
     {
         // Création des catégories
-        App\Category::create([
-            "gender" => "Homme"
-        ]);
+        $categories_array = ["Homme", "Femme"];
+        foreach($categories_array as $category) {
+            App\Category::create([
+                "gender" => $category,
+            ]);
+        };
 
-        App\Category::create([
-            "gender" => "Femme"
-        ]);
-
-        App\Size::create([
-            "value" => "XS",
-        ]);
-        App\Size::create([
-            "value" => "S",
-        ]);
-        App\Size::create([
-            "value" => "M",
-        ]);
-        App\Size::create([
-            "value" => "L",
-        ]);
-        App\Size::create([
-            "value" => "XL",
-        ]);
+        // Création des tailles
+        $size_array = ["XS","S","M","L","XL"];
+        foreach($size_array as $size) {
+            App\Size::create([
+                "value" => $size,
+            ]);
+        };
 
         factory(App\Product::class, 20)->create()->each(function ($product) {
             // associe une categorie à un produit
             $category = App\Category::find(rand(1, 2));
 
-            // pour chaque $product on lui associe un genre en particulier
+            // pour chaque $product on lui associe une categorie en particulier
             $product->category()->associate($category);
 
             $files = Storage::allFiles($category->gender);
@@ -50,25 +41,18 @@ class ProductTableSeeder extends Seeder
             $file = $files[$fileIndex];
 
             // ajout des images
-
             $product->image()->create([
                 'link' => $file
             ]);
 
-            // récupération des id des auteurs à partir de la méthode pluck d'Eloquent
-            // les méthodes du pluck shuffle et slice permettent de mélanger et récupérer un certain
-            // nombre de 3 à partir de l'indice 0, comme ils sont mélangés à chaque fois qu'un livre est crée
-            // La méthode all permet de faire la requête et de récupérer le résultat sous forme d'un tableau
+            // Créer un tableau aléatoire de tailles 
             $sizes = App\Size::pluck('id')->shuffle()->slice(0, rand(1, 5))->all();
             $filtered_sizes = array_values(array_sort($sizes, function ($value) {
                 return $value;
             }));
 
-            // Il faut se mettre maintenant en relation avec les auteurs (relation ManyToMany)
-            // et attacher les id des auteurs dans la table de liaison.
+            // Attache les tailles au produit
             $product->sizes()->attach($filtered_sizes);
-
-
             $product->save(); // il faut sauvegarder l'association pour faire persister en base de données
         });
     }
